2a4e6bd04bdf9da7d905804e7d4a7a29ba80c001
Jupiter <jupiter@admin-6s-iMac.local>
2025-10-09 21:28:53 +0800

    lessen day 04,05

---
+++ lessen_day_05/loop.js
@@ -0,0 +1,17 @@
+for(let number = 1; number <= 5; number++){
+    console.log(number);
+}
+
+console.log("Ex_2");
+
+let fruits = ["apple", "Banana", "Orange"];
+for(const fruit of fruits){
+    console.log(fruit);
+}
+
+    console.log(fruits);
+
+
+
+
+
