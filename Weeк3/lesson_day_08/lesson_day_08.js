9433e9232a9f9ebb1bc5e39cea1a12366c91e688
abr99069966 <abr99069966@gmail.com>
2025-10-15 18:30:30 +0800

    20251015
    20251015

---
+++ Weeк3/lesson_day_08/lesson_day_08.js
@@ -0,0 +1,19 @@
+// Дасгал 01.
+console.log("Дасгал 01");
+
+function greetByName(name) {
+    console.log(`Сайн уу, ${name}!`);
+}
+greetByName("Дорж");
+greetByName("Сараа");
+
+// Дасгал 02.
+console.log("Дасгал 02");
+
+function addTwoNumbers() {
+    let num1 = 10;
+    let num2 = 25;
+    let S = num1 + num2;
+    console.log(num1, '+', num2,  );
+}
+addTwoNumbers()
