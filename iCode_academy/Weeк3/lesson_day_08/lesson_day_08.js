457b73d158a941abbee52bf25b7257d2df06de66
abr99069966 <abr99069966@gmail.com>
2025-10-16 17:21:50 +0800

    Update lesson_day_08.js

--- iCode_academy/Weeк3/lesson_day_08/lesson_day_08.js
+++ iCode_academy/Weeк3/lesson_day_08/lesson_day_08.js
@@ -1,19 +1,43 @@
 // Дасгал 01.
-console.log("Дасгал 01");
+// console.log("Дасгал 01");
 
-function greetByName(name) {
-    console.log(`Сайн уу, ${name}!`);
-}
-greetByName("Дорж");
-greetByName("Сараа");
+// function greetByName(name) {
+//     console.log(`Сайн уу, ${name}!`);
+// }
+// greetByName("Дорж");
+// greetByName("Сараа");
 
 // Дасгал 02.
-console.log("Дасгал 02");
+// console.log("Дасгал 02");
 
-function addTwoNumbers() {
-    let num1 = 10;
-    let num2 = 25;
-    let S = num1 + num2;
-    console.log(num1, '+', num2,  );
+// function addTwoNumbers(num1, num2) {
+//      console.log(num1 + num2);
+// }
+// addTwoNumbers(10, 25);
+// addTwoNumbers( 100, -50)
+
+// Дасгал 03.
+// console.log("Дасгал 03");
+
+// function printFavoriteColor(color){
+//     console.log("Миний дуртай өнгө бол", color );
+// }
+// printFavoriteColor("Цэнхэр")
+
+// Дасгал 04.
+// console.log("Дасгал 04");
+
+// function calculateArea(length, width) {
+//     let = length;
+//     let = width;
+//     let sum = length * width;
+//     console.log("Урт=", length, "Өргөн=", width, "үед талбай нь", sum, "байна.");
+// }
+// calculateArea(10, 5)
+
+// Дасгал 05.
+console.log("Дасгал 05");
+
+function repeatMessage(message, times){
+    
 }
-addTwoNumbers()
