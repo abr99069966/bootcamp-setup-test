eafecca4175743375cca78abfe8c89011da9e275
abr99069966 <abr99069966@gmail.com>
2025-12-15 10:50:17 +0800

    251212

--- iCode_academy/Week11/findmovies/src/App.jsx
+++ iCode_academy/Week11/findmovies/src/App.jsx
@@ -8,15 +8,17 @@ function App() {
 
   return (
     <>
-      <div className='flex'>
+      <div className='h-7 flex m-5 pl-20 pr-20 gap-10' >
         <div className='flex gap-3 items-center h-[26px]' >
           <img className='w-[26px] h-[26px] '
           src={logoImage} alt="logo" />
-          <h1 className='h-[14px] justify-center '>FindMovies</h1>
+          <h1 className='text-white h-[14px] justify-center font-semibold '>FindMovies</h1>
         </div>
-        <div>
-          <img src={searchIcon} alt="search" />
-          <input type="text" />
+        <div className=' h-7 w-[981] flex border rounded-lg bg-white '>
+          <img className='m-1' src={searchIcon} alt="search" />
+          <input type="text" 
+          placeholder='Search FindMovies'
+          />
 
         </div>
       </div>
