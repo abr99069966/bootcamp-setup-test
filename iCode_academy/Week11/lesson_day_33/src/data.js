3245c816822b4ed63ce4e6223bfb25a30fce65cb
abr99069966 <abr99069966@gmail.com>
2025-12-16 09:49:18 +0800

    251216

---
+++ iCode_academy/Week11/lesson_day_33/src/data.js
@@ -0,0 +1,101 @@
+const movies = [
+    {
+      "id": 1,
+      "title": "Everything Everywhere All at Once",
+      "rating": 8.0,
+      "type": "Movie",
+      "poster": ""
+    },
+    {
+      "id": 2,
+      "title": "The Menu",
+      "rating": 7.2,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_THE_MENU_POSTER"
+    },
+    {
+      "id": 3,
+      "title": "Avatar: The Way of Water",
+      "rating": 7.8,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_AVATAR_POSTER"
+    },
+    {
+      "id": 4,
+      "title": "Glass Onion: Knives Out",
+      "rating": 7.2,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_GLASS_ONION_POSTER"
+    },
+    {
+      "id": 5,
+      "title": "Puss in Boots: The Last Wish",
+      "rating": 7.9,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_PUSS_IN_BOOTS_POSTER"
+    },
+    {
+      "id": 6,
+      "title": "M3GAN",
+      "rating": 6.5,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_M3GAN_POSTER"
+    },
+    {
+      "id": 7,
+      "title": "The Pale Blue Eye",
+      "rating": 6.5,
+      "type": "Movie",
+      "poster": "URL_OR_LOCAL_PATH_TO_PALE_BLUE_EYE_POSTER"
+    },
+    {
+      "id": 101,
+      "title": "The Amazing Maurice",
+      "rating": 6.3,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_MAURICE_POSTER"
+    },
+    {
+      "id": 102,
+      "title": "Babylon",
+      "rating": 7.6,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_BABYLON_POSTER"
+    },
+    {
+      "id": 103,
+      "title": "Mummies",
+      "rating": 6.6,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_MUMMIES_POSTER"
+    },
+    {
+      "id": 104,
+      "title": "BTS: Yet to Come in Cinemas",
+      "rating": 7.6,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_BTS_POSTER"
+    },
+    {
+      "id": 105,
+      "title": "The Banshees of Inisherin",
+      "rating": 7.8,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_BANHEES_POSTER"
+    },
+    {
+      "id": 106,
+      "title": "A Man Called Otto",
+      "rating": 7.6,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_OTTO_POSTER"
+    },
+    {
+      "id": 107,
+      "title": "Magic Mike's Last Dance",
+      "rating": 5.9,
+      "type": "Premiere",
+      "poster": "URL_OR_LOCAL_PATH_TO_MAGIC_MIKE_POSTER"
+    }
+
+];
